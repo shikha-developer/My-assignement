d70c25a61c064a859caefeccd57a96fecdab0acc
shikha <shkmks8@gmail.com>
2019-07-02 13:30:11 +0530

    Observables add for dashboard component

---
+++ My-project/src/app/services/userdetail.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {  HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserDetailsConfig } from '../users.config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserdetailService {
+ dataUrl: string  = "../../assets/userDetail.json";
+  constructor(private _http: HttpClient) { 
+
+  }
+  getUserDetails():Observable<UserDetailsConfig[]>{
+    return this._http.get<UserDetailsConfig[]>(this.dataUrl);
+  }
+}
